e12e34967fda681ce946f7b75d4ab08e42950adf
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2024-09-30 20:44:15 -0300

    Adding missing files

---
+++ src/main/java/Game.java
@@ -0,0 +1,29 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private final TerminalScreen screen;
+    private int x = 10;
+    private int y = 10;
+
+    public Game(int  width, int height) throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, height)).createTerminal();
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);   // we don't need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
+    }
+
+    private void draw() throws IOException{
+
+    }
+
+    public void run() throws IOException{
+
+    }
+}
