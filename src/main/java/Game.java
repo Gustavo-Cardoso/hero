65cabd5cb5e9d1a46bc68086ca699651e90c0028
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2024-10-01 10:41:46 -0300

    Reading keystrokes and The Hero Class

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,6 @@
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
@@ -8,22 +9,49 @@ import java.io.IOException;
 
 public class Game {
     private final TerminalScreen screen;
-    private int x = 10;
-    private int y = 10;
-
+    private int x;
+    private int y;
+    private final Hero hero;
     public Game(int  width, int height) throws IOException {
         Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, height)).createTerminal();
         screen = new TerminalScreen(terminal);
+        hero = new Hero(10, 10);
         screen.setCursorPosition(null);   // we don't need a cursor
         screen.startScreen();             // screens must be started
         screen.doResizeIfNecessary();     // resize screen if necessary
     }
-
-    private void draw() throws IOException{
-
+    private void draw() throws IOException {
+        screen.clear();
+        hero.draw(screen);
+        screen.refresh();
     }
-
-    public void run() throws IOException{
-
+    public void run() throws IOException {
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                screen.close();
+            if (key.getKeyType() == KeyType.EOF)
+                break;
+            processKey(key);
+        }
+    }
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+        String keyT = key.getKeyType().toString();
+        switch (keyT) {
+            case "ArrowUp":
+                hero.moveUp();
+                break;
+            case "ArrowDown":
+                hero.moveDown();
+                break;
+            case "ArrowLeft":
+                hero.moveLeft();
+                break;
+            case "ArrowRight":
+                hero.moveRight();
+                break;
+        }
     }
 }
